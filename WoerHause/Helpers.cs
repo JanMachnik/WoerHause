f7d166789df932fc33832e59af1b0a431302b262
Jan <Jan@LAPTOP-VTD1OEOJ>
2022-07-12 19:30:09 +0200

    Add project files.

---
+++ WoerHause/Helpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WoerHause
+{
+    public class Helpers
+    {
+        public int GiveMeNumber()
+        {
+            SomeStructure stru = new SomeStructure();
+
+            int number = 5;
+
+            return number;
+
+        }
+
+        public enum ItemType
+        {
+            Glosery = 1,
+            Clothing,
+            Electronics,
+
+        }
+        public struct SomeStructure
+        {
+            private int numberForStructure;
+            private string naemOfStructure;
+
+            public SomeStructure(int number, string naeme)
+
+            {
+                numberForStructure = number;
+
+
+            }
+
+        }
+    }
+}
---
+++ WoerHause/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Warehause
+{
+    class Program
+    {
+        //Stała
+        public const string FILE_NAME = "C\\WarehouseFiles\\ImportFiles.xlsx";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to warehause app!");
+            Console.WriteLine("Please let me know what you want to do:");
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Remove Item");
+            Console.WriteLine("3. Check Item");
+            Console.WriteLine("Press 1, 2 or 3...");
+
+            string operation = Console.ReadLine();
+
+            Console.WriteLine($"You have chosen option nomber: {operation}");
+ 
+            int chosenOperation = 0;
+
+            Int32.TryParse(operation, out chosenOperation);
+
+            /*Console.WriteLine("Select item category");
+            Console.WriteLine("1. Glosery");
+            Console.WriteLine("2. Clothing");
+            Console.WriteLine("3. Electronics");
+
+            string category = Console.ReadLine();
+
+            ItemType chosenCategory;
+
+            Enum.TryParse(category, out chosenCategory);
+
+            Item item = new Item() { Id = 1, Name = "Apple" };
+ 
+        }   
+        
+
+
+
+       
+    }
+}
